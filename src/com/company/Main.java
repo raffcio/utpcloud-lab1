f9372eb8e0a737caf89602b2fe4ef819d9b97368
raffcio <rafalnowak93@gmail.com>
2021-03-06 21:01:13 +0100

    zmiana nazwy projektu na nazwiska

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 		// komentarz 1
 		// komentarz 2
-		System.out.println("nazwa projektu");
+		System.out.println(new DEV1().author + " " + new DEV2().author);
     }
 }
